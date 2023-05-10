666fddba89385d7a8ceda5935d71149f4d74cb81
celdering-trm <christian.eldering@trmlabs.com>
2023-05-09 21:37:11 -0400

    add scripts

---
+++ nvidia-driver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get install -y wget
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/525.116.04/NVIDIA-Linux-x86_64-525.116.04.run
+
+chmod +x NVIDIA-Linux-x86_64-525.116.04.run
